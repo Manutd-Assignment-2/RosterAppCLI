e3c787c2b61f41329c32d7f00261ed1aff17a434
Dip <113352667+izDeLaMo@users.noreply.github.com>
2025-09-25 00:33:33 +0000

    models

--- App/models/user.py
+++ App/models/user.py
@@ -6,6 +6,10 @@ class User(db.Model):
     username =  db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
 
+    __mapper_args__ = {
+        "polymorphic_identity": "user",
+        
+        }   
     def __init__(self, username, password):
         self.username = username
         self.set_password(password)
@@ -23,4 +27,28 @@ class User(db.Model):
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
+    
+class Admin(User):
+    adminID = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "admin",
+        
+        }
+    def __init__(self, username, password):
+        super().__init__(username, password)
+        self.adminID = self.id
+
+class Staff(User):
+    staffID = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    
+    __mapper_args__ = {
+        "polymorphic_identity": "staff",
+        
+        }
+    def __init__(self, username, password):
+        super().__init__(username, password)
+        self.staffID = self.id
+        
+
 

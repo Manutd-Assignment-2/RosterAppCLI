81ca4d002d0e72231499a33048cfa3bcd6c43794
Dip <113352667+izDeLaMo@users.noreply.github.com>
2025-10-20 23:06:43 +0000

    models refactor fix

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -2,4 +2,4 @@ from App.models.user import User
 from App.models.admin import Admin
 from App.models.staff import Staff
 from App.models.schedule import Schedule
-from App.models.schedule import Shift
+from App.models.shift import Shift

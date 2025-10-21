210a66c8e66d8248896450f2a06bb9469fc835f1
Dip <113352667+izDeLaMo@users.noreply.github.com>
2025-10-21 02:42:07 +0000

    view test fix

--- App/views/user.py
+++ App/views/user.py
@@ -32,8 +32,8 @@ def get_users_action():
 @user_views.route('/api/users', methods=['POST'])
 def create_user_endpoint():
     data = request.json
-    user = create_user(data['username'], data['password'])
-    return jsonify({'message': f"user {user.username} created with id {user.id}"})
+    user = create_user(data['username'], data['password'], data['role']) 
+    return jsonify({'message': f"user {user.username} created with id {user.id}"}), 201
 
 @user_views.route('/static/users', methods=['GET'])
 def static_user_page():
